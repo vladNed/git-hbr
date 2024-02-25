47e7ab43e3560418826581a8fa7dd85fe376dbda
Vlad Nedelcu <nedelcuvd@gmail.com>
2024-02-25 13:52:37 +0200

    fmt & tests

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,17 @@
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "git hbr", about = "Manage local branches using hash identifiers")]
+#[structopt(
+    name = "git hbr",
+    about = "Manage local branches using hash identifiers"
+)]
 pub struct Opt {
-
     pub branch_hashes: Vec<String>,
 
-    #[structopt(short = "l", long = "list", help = "List all local branches with their hash identifiers")]
+    #[structopt(
+        short = "l",
+        long = "list",
+        help = "List all local branches with their hash identifiers"
+    )]
     pub listing: bool,
 }
